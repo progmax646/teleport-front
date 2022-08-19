bbb87690eb270ebb635f2ebe790720e08bdd8114
Maksim <rasskazovmaksim97@gmai.com>
2022-08-19 14:46:03 +0500

    add folder section in dashboard

---
+++ components/dashboard/MaterialComponent.vue
@@ -0,0 +1,106 @@
+<template>
+<div>
+  <div class="page-head">
+    <div class="d-flex flex-column">
+      <h3>Получи 15 материалов бесплатно</h3>
+      <p>Всего 12 страниц</p>
+    </div>
+    <div>
+      <button class="btn btn-red"> <i>
+        <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M4.99995 1.00378V8.99617" stroke="white" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M9 4.9997H1" stroke="white" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+
+      </i>Создать страницу</button>
+    </div>
+  </div>
+  <div class="material">
+    <div class="material__item">
+      <div class="info">
+        <div class="info__head">
+          <span>1</span>
+          <h5>Моя личная страница</h5>
+        </div>
+        <div class="info__inner">
+          <div class="info-profile">
+            <div class="ava">
+              <img src="@/static/img/default-ava.png" alt="">
+            </div>
+            <div class="info">
+              <div class="info-1">
+                <p>blogger.man</p>
+                <span class="status">Активная</span>
+              </div>
+              <div class="info-2">
+                <p>id: 15-materials</p>
+                <p>domain: instateleport.ru</p>
+              </div>
+            </div>
+            <div class="statistics">
+              <div class="statistics__item">
+                <p>Подписчики</p>
+                <div class="info">
+                  <div class="photo red">
+                    <i><svg width="10" height="13" viewBox="0 0 10 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M7.7433 6.18823C7.02709 6.86532 6.05411 7.27157 5.00006 7.27157C3.94601 7.27157 2.97303 6.86532 2.25682 6.18823C0.932492 7.25802 0.067627 8.92365 0.067627 10.2101C0.067627 12.2549 0.81087 12.932 5.00006 12.932C9.18925 12.932 9.93249 12.2549 9.93249 10.2101C9.93249 8.92365 9.06763 7.25802 7.7433 6.18823Z" fill="#FD3162"/>
+                      <path d="M4.99997 6.37804C6.73892 6.37804 8.14862 4.96541 8.14862 3.22284C8.14862 1.48026 6.73892 0.067627 4.99997 0.067627C3.26102 0.067627 1.85132 1.48026 1.85132 3.22284C1.85132 4.96541 3.26102 6.37804 4.99997 6.37804Z" fill="#FD3162"/>
+                    </svg>
+                    </i>
+                  </div>
+                  <span>250</span>
+                </div>
+              </div>
+              <div class="statistics__item">
+                <p>Подписчики</p>
+                <div class="info">
+                  <div class="photo purple">
+                    <i><svg width="10" height="13" viewBox="0 0 10 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M7.7433 6.18823C7.02709 6.86532 6.05411 7.27157 5.00006 7.27157C3.94601 7.27157 2.97303 6.86532 2.25682 6.18823C0.932492 7.25802 0.067627 8.92365 0.067627 10.2101C0.067627 12.2549 0.81087 12.932 5.00006 12.932C9.18925 12.932 9.93249 12.2549 9.93249 10.2101C9.93249 8.92365 9.06763 7.25802 7.7433 6.18823Z" fill="#FD3162"/>
+                      <path d="M4.99997 6.37804C6.73892 6.37804 8.14862 4.96541 8.14862 3.22284C8.14862 1.48026 6.73892 0.067627 4.99997 0.067627C3.26102 0.067627 1.85132 1.48026 1.85132 3.22284C1.85132 4.96541 3.26102 6.37804 4.99997 6.37804Z" fill="#FD3162"/>
+                    </svg>
+                    </i>
+                  </div>
+                  <span>250</span>
+                </div>
+              </div>
+              <div class="statistics__item">
+                <p>Подписчики</p>
+                <div class="info">
+                  <div class="photo orange">
+                    <i>
+                      <svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <g clip-path="url(#clip0_3154_15575)">
+                        <path d="M5.60571 6.41863V1.21537C5.60663 1.17545 5.59962 1.13574 5.58508 1.09855C5.57055 1.06136 5.54877 1.02742 5.52103 0.998709C5.49328 0.969993 5.46011 0.947069 5.42343 0.931266C5.38676 0.915463 5.34732 0.907095 5.30739 0.906647C1.09276 1.08703 -1.8835 5.92258 1.40495 10.4113L1.59227 10.5986C6.04972 13.887 10.8992 10.9212 11.1142 6.73082C11.1143 6.64979 11.0828 6.57192 11.0265 6.51366C10.9702 6.45541 10.8934 6.42133 10.8124 6.41863H5.60918H5.60571Z" fill="#BC31FD"/>
+                        <path d="M6.50751 0.632637V5.51676H11.3639C11.4434 5.51721 11.5223 5.50194 11.5959 5.47181C11.6695 5.44168 11.7365 5.3973 11.7929 5.3412C11.8493 5.28511 11.8941 5.21842 11.9246 5.14496C11.9552 5.07151 11.9709 4.99274 11.9709 4.91318C11.9709 4.8889 11.9709 4.86115 11.9709 4.83687C11.8165 3.62004 11.2634 2.48863 10.398 1.61943C9.53253 0.750228 8.40354 0.192196 7.1874 0.032529C7.10811 0.0228397 7.0277 0.0288913 6.95076 0.0503372C6.87382 0.0717831 6.80187 0.108202 6.73903 0.157506C6.6762 0.206809 6.6237 0.268029 6.58457 0.337657C6.54544 0.407284 6.52043 0.48395 6.51097 0.563261C6.50809 0.586267 6.50693 0.609458 6.50751 0.632637Z" fill="#BC31FD"/>
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_3154_15575">
+                          <rect width="12" height="12.0325" fill="white"/>
+                        </clipPath>
+                      </defs>
+                    </svg>
+
+                    </i>
+                  </div>
+                  <span>250</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "MaterialComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,20 @@
   <div>
     <div class="d-flex flex-column page">
       <FolderComponent/>
+      <MaterialComponent/>
     </div>
   </div>
 </template>
 
 <script>
 import FolderComponent from "../components/dashboard/FolderComponent";
+import MaterialComponent from "../components/dashboard/MaterialComponent";
 export default {
   name: 'IndexPage',
   layout: 'default',
   components: {
-    FolderComponent
+    FolderComponent,
+    MaterialComponent
   }
 }
 </script>
